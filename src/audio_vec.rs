a1f5f3fb774e63be3339b85dd8d3e85250be203a
MikuroXina <ryosukadnak@gmail.com>
2022-10-20 16:05:16 +0900

    Add from_pcm

--- src/audio_vec.rs
+++ src/audio_vec.rs
@@ -179,3 +179,17 @@ impl AudioVec {
         buf1
     }
 }
+
+impl AudioVec {
+    #[inline]
+    pub fn from_pcm(path: &[u16]) -> Self {
+        // プリエンファシス
+        let emphasized = path.windows(2).map(|win| {
+            ModInt998244353::new((win[0] as i32 + 40 * (win[1] as i32 - win[0] as i32)) as u32)
+        });
+
+        Self {
+            vec: emphasized.collect(),
+        }
+    }
+}
