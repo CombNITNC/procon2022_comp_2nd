ea39699ca59e3a1ada94e1cfcc81d1923992ee14
MikuroXina <ryosukadnak@gmail.com>
2022-10-20 18:42:19 +0900

    Add find_points

--- src/solve.rs
+++ src/solve.rs
@@ -44,6 +44,7 @@ impl Loss {
     /// 2 乗ノルムを用いた損失関数
     ///
     /// `problem_voice` は `card_voices` のうちからいくつかが選ばれて, 時間をずらして重ね合わせたもの
+    #[inline]
     pub fn evaluate(&self, problem_voice: &AudioVec, point: InspectPoint) -> u32 {
         (problem_voice.squared_norm()
             - ModInt998244353::new(2)
@@ -58,5 +59,57 @@ impl Loss {
                 ..point
             }])
             .as_u32()
+    }
+
+    pub fn find_points(&self, solutions: usize, problem_voice: &AudioVec) -> Vec<InspectPoint> {
+        let points_by_loss: Vec<_> = {
+            let mut points: Vec<_> = self
+                .card_voices
+                .iter()
+                .enumerate()
+                .flat_map(|(using, card_voice)| {
+                    (-(card_voice.len() as isize)..problem_voice.len() as isize).map(move |delay| {
+                        InspectPoint {
+                            using_voice: using,
+                            delay,
+                        }
+                    })
+                })
+                .collect();
+            points.sort_by_key(|&p| self.evaluate(problem_voice, p));
+            points
+        };
+        let first_answer = points_by_loss[..solutions].to_vec();
+
+        // この最初に見つけた解が問題に一致するかどうか検算
+        if self.validate(problem_voice, &first_answer) {
+            return first_answer;
+        }
+
+        // 違うようなので, 最初の解から 1 つだけ取り除いて別の解を探す
+        for &next_candidate in &points_by_loss[solutions..] {
+            for to_remove in 0..first_answer.len() {
+                let next_answer = {
+                    let mut list = first_answer.clone();
+                    list[to_remove] = next_candidate;
+                    list
+                };
+                if self.validate(problem_voice, &next_answer) {
+                    return next_answer;
+                }
+            }
+        }
+
+        todo!()
+    }
+
+    fn validate(&self, problem_voice: &AudioVec, first_answer: &[InspectPoint]) -> bool {
+        let mut composed = AudioVec::default();
+        composed.resize(problem_voice.len());
+        for &InspectPoint { using_voice, delay } in first_answer {
+            composed.add_assign(&self.card_voices[using_voice].delayed(delay));
+        }
+        const THRESHOLD: u32 = 100;
+        problem_voice.sub(&composed).squared_norm().as_u32() < THRESHOLD
     }
 }
