77b742ee3732bc133e30a36d29236641b0e285e4
MikuroXina <ryosukadnak@gmail.com>
2022-10-22 04:08:56 +0900

    Add run_solver

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 use std::{collections::HashMap, fs::File, io, path::PathBuf};
 
-use self::{audio_vec::AudioVec, request::net::NetRequester, solve::card_voice::CardVoiceIndex};
+use self::{
+    audio_vec::AudioVec,
+    request::{net::NetRequester, Answer, Requester},
+    solve::{card_voice::CardVoiceIndex, Loss},
+};
 
 mod audio_vec;
 mod request;
@@ -11,11 +15,18 @@ fn main() -> anyhow::Result<()> {
 
     let endpoint = std::env::var("ENDPOINT")?;
     let token = std::env::var("TOKEN")?;
+    let debug = std::env::var("DEBUG")?;
 
     let all_jk = load_all_jk()?;
-    let requester = NetRequester::new(&endpoint, &token);
+    const MIN_VOICE_LEN: usize = 48000 / 2;
+    let loss = Loss::new(all_jk, MIN_VOICE_LEN);
 
-    Ok(())
+    if debug.as_str().trim() == "True" {
+        todo!()
+    } else {
+        let requester = NetRequester::new(&endpoint, &token);
+        run_solver(&loss, &requester)
+    }
 }
 
 fn load_all_jk() -> io::Result<HashMap<CardVoiceIndex, AudioVec>> {
@@ -31,4 +42,22 @@ fn load_all_jk() -> io::Result<HashMap<CardVoiceIndex, AudioVec>> {
         map.insert(idx, AudioVec::from_pcm(&pcm));
     }
     Ok(map)
+}
+
+fn run_solver(loss: &Loss, requester: &impl Requester) -> anyhow::Result<()> {
+    let problem_info = requester.get_problem()?;
+    let chunks = requester.get_chunks(1)?;
+    let chunk = &chunks[0];
+
+    let points = loss.find_points(problem_info.data as usize, chunk);
+
+    requester.post_answer(&Answer {
+        problem_id: problem_info.id,
+        answers: points
+            .into_iter()
+            .map(|p| p.using_voice.into_answer_string())
+            .collect(),
+    })?;
+
+    Ok(())
 }
