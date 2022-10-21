5747a0de1454e803f95768357c66f7695f25556c
MikuroXina <ryosukadnak@gmail.com>
2022-10-22 04:08:10 +0900

    Fix to_string as 1-indexed

--- src/solve/card_voice.rs
+++ src/solve/card_voice.rs
@@ -4,9 +4,9 @@ pub struct CardVoiceIndex(u8);
 impl std::fmt::Display for CardVoiceIndex {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         if self.0 < 44 {
-            write!(f, "E{:02}", self.0)
+            write!(f, "E{:02}", self.0 + 1)
         } else {
-            write!(f, "J{:02}", self.0 - 44)
+            write!(f, "J{:02}", self.0 + 1 - 44)
         }
     }
 }
@@ -21,5 +21,14 @@ impl CardVoiceIndex {
     #[inline]
     pub fn all() -> impl Iterator<Item = CardVoiceIndex> {
         (0..88).map(Self::new)
+    }
+
+    #[inline]
+    pub fn into_answer_string(self) -> String {
+        if self.0 < 44 {
+            (self.0 + 1).to_string()
+        } else {
+            (self.0 + 1 - 44).to_string()
+        }
     }
 }
