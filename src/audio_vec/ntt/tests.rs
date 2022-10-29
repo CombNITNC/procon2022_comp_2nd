5909449dda0f43adfd0a0dbb7087f66148c19461
MikuroXina <ryosukadnak@gmail.com>
2022-10-29 09:39:44 +0900

    Add convolution2

--- src/audio_vec/ntt/tests.rs
+++ src/audio_vec/ntt/tests.rs
@@ -1,10 +1,11 @@
+//! From: https://judge.yosupo.jp/problem/convolution_mod
+
 use crate::audio_vec::{mod_int::ModInt998244353, AudioVec};
 
 use super::Ntt;
 
 #[test]
-/// From: https://judge.yosupo.jp/problem/convolution_mod
-fn convolution() {
+fn convolution1() {
     let a: Vec<_> = [1, 2, 3, 4].into_iter().map(ModInt998244353::new).collect();
     let b: Vec<_> = [5, 6, 7, 8, 9]
         .into_iter()
@@ -20,5 +21,19 @@ fn convolution() {
         .into_iter()
         .map(ModInt998244353::new)
         .collect();
+    assert_eq!(out.vec, expected);
+}
+
+#[test]
+fn convolution2() {
+    let a: Vec<_> = [10000000].into_iter().map(ModInt998244353::new).collect();
+    let b: Vec<_> = [10000000].into_iter().map(ModInt998244353::new).collect();
+
+    let a_audio = AudioVec { vec: a };
+    let b_audio = AudioVec { vec: b };
+    let ntt = Ntt::new();
+    let out = a_audio.convolution(&b_audio, &ntt);
+
+    let expected: Vec<_> = [871938225].into_iter().map(ModInt998244353::new).collect();
     assert_eq!(out.vec, expected);
 }
