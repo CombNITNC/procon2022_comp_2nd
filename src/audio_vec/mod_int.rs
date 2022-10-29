0567d2714cd4076f42512c593b22cf9cd5f94862
MikuroXina <ryosukadnak@gmail.com>
2022-10-29 18:06:07 +0900

    Remove wrapping_proc_macro

--- src/audio_vec/mod_int.rs
+++ src/audio_vec/mod_int.rs
@@ -1,6 +1,22 @@
+use std::num::Wrapping;
+
 use num::{One, Zero};
 use serde::{Deserialize, Serialize};
-use wrapping_proc_macro::wrapping;
+
+trait WrappingCast<T> {
+    fn cast<U: From<T>>(self) -> Wrapping<U>;
+    fn try_cast<U: TryFrom<T>>(self) -> Result<Wrapping<U>, <T as TryInto<U>>::Error>;
+}
+
+impl<T> WrappingCast<T> for Wrapping<T> {
+    fn cast<U: From<T>>(self) -> Wrapping<U> {
+        Wrapping::<U>(self.0.into())
+    }
+
+    fn try_cast<U: TryFrom<T>>(self) -> Result<Wrapping<U>, <T as TryInto<U>>::Error> {
+        Ok(Wrapping(self.0.try_into()?))
+    }
+}
 
 const fn find_neg_inv(n: u32) -> u32 {
     let n = n as i64;
@@ -10,12 +26,10 @@ const fn find_neg_inv(n: u32) -> u32 {
 const fn find_r(n: u32) -> u32 {
     let n = n as i64;
     let mut ret = n;
-    wrapping! {
-        ret *= 2i64 - n * ret;
-        ret *= 2i64 - n * ret;
-        ret *= 2i64 - n * ret;
-        ret *= 2i64 - n * ret;
-    }
+    ret = ret.wrapping_mul(2i64.wrapping_sub(n.wrapping_mul(ret)));
+    ret = ret.wrapping_mul(2i64.wrapping_sub(n.wrapping_mul(ret)));
+    ret = ret.wrapping_mul(2i64.wrapping_sub(n.wrapping_mul(ret)));
+    ret = ret.wrapping_mul(2i64.wrapping_sub(n.wrapping_mul(ret)));
     ret as u32
 }
 
@@ -27,7 +41,7 @@ pub type ModInt924844033 = ModInt<924844033>;
     Debug, Default, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash, Serialize, Deserialize,
 )]
 #[repr(transparent)]
-pub struct ModInt<const MOD: u32>(u32);
+pub struct ModInt<const MOD: u32>(Wrapping<u32>);
 
 impl<const MOD: u32> From<ModInt<MOD>> for u32 {
     #[inline]
@@ -47,7 +61,7 @@ impl<const MOD: u32> ModInt<MOD> {
 
     #[inline]
     pub const fn as_u32(self) -> u32 {
-        self.0
+        self.0 .0
     }
 
     /// この剰余である整数の法. 素数であるとする.
@@ -60,9 +74,9 @@ impl<const MOD: u32> ModInt<MOD> {
     #[inline]
     pub fn pow(mut self, mut exp: u32) -> Self {
         if exp == 0 {
-            return Self(1);
+            return Self::new(1);
         }
-        let mut y = Self(1);
+        let mut y = Self::new(1);
         while 0 < exp {
             if exp % 2 == 1 {
                 y *= self;
@@ -79,23 +93,17 @@ impl<const MOD: u32> ModInt<MOD> {
     }
 
     #[inline]
-    pub fn factorial(self) -> Self {
-        let mut result = Self(1);
-        for x in 2..=self.0 {
-            result *= Self(x);
-        }
-        result
-    }
-
-    #[inline]
-    pub const fn reduce(t: u64) -> Self {
-        let n = Self::N as u64;
-        let r = Self::R as u64;
-        let n_prime = Self::N_PRIME as u64;
-        let t_prime = wrapping! {
-            (t + (t * n_prime % r) * n) / r
-        };
-        Self(if n <= t_prime { t_prime - n } else { t_prime } as u32)
+    pub fn reduce(t: u64) -> Self {
+        let t = Wrapping(t);
+        let n = Wrapping(Self::N as u64);
+        let r = Wrapping(Self::R as u64);
+        let n_prime = Wrapping(Self::N_PRIME as u64);
+        let t_prime = (t + (t * n_prime % r) * n) / r;
+        Self(
+            (if n <= t_prime { t_prime - n } else { t_prime })
+                .try_cast()
+                .unwrap(),
+        )
     }
 }
 
@@ -133,13 +141,9 @@ impl<const MOD: u32> std::ops::Add for ModInt<MOD> {
 impl<const MOD: u32> std::ops::AddAssign for ModInt<MOD> {
     #[inline]
     fn add_assign(&mut self, rhs: Self) {
-        wrapping! {
-            self.0 += rhs.0 - 2u32 * MOD;
-        }
-        if (self.0 as i32) < 0 {
-            wrapping! {
-                self.0 += 2u32 * MOD;
-            }
+        self.0 += rhs.0 - Wrapping(2 * MOD);
+        if (self.0 .0 as i32) < 0 {
+            self.0 += Wrapping(2 * MOD);
         }
     }
 }
@@ -157,13 +161,9 @@ impl<const MOD: u32> std::ops::Sub for ModInt<MOD> {
 impl<const MOD: u32> std::ops::SubAssign for ModInt<MOD> {
     #[inline]
     fn sub_assign(&mut self, rhs: Self) {
-        wrapping! {
-            self.0 -= rhs.0;
-        }
-        if (self.0 as i32) < 0 {
-            wrapping! {
-                self.0 += 2u32 * MOD;
-            }
+        self.0 -= rhs.0;
+        if (self.0 .0 as i32) < 0 {
+            self.0 += Wrapping(2 * MOD);
         }
     }
 }
@@ -181,7 +181,7 @@ impl<const MOD: u32> std::ops::Mul for ModInt<MOD> {
 impl<const MOD: u32> std::ops::MulAssign for ModInt<MOD> {
     #[inline]
     fn mul_assign(&mut self, rhs: Self) {
-        *self = Self::reduce(self.0 as u64 * rhs.0 as u64);
+        *self = Self::reduce((self.0.cast::<u64>() * rhs.0.cast::<u64>()).0);
     }
 }
 
@@ -208,7 +208,7 @@ impl<const MOD: u32> std::ops::Neg for ModInt<MOD> {
 
     #[inline]
     fn neg(self) -> Self::Output {
-        Self(0) - self
+        Self::new(0) - self
     }
 }
 
@@ -218,7 +218,7 @@ impl<const MOD: u32> std::iter::Sum for ModInt<MOD> {
     where
         I: Iterator<Item = Self>,
     {
-        iter.fold(Self(0), |a, b| a + b)
+        iter.fold(Self::new(0), |a, b| a + b)
     }
 }
 
@@ -228,25 +228,25 @@ impl<const MOD: u32> std::iter::Product for ModInt<MOD> {
     where
         I: Iterator<Item = Self>,
     {
-        iter.fold(Self(1), |a, b| a * b)
+        iter.fold(Self::new(1), |a, b| a * b)
     }
 }
 
 impl<const MOD: u32> Zero for ModInt<MOD> {
     #[inline]
     fn zero() -> Self {
-        Self(0)
+        Self::new(0)
     }
 
     #[inline]
     fn is_zero(&self) -> bool {
-        self.0 == 0
+        self.0 .0 == 0
     }
 }
 
 impl<const MOD: u32> One for ModInt<MOD> {
     #[inline]
     fn one() -> Self {
-        Self(1)
+        Self::new(1)
     }
 }
