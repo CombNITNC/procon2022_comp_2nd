cf6b4625233052463234ab1fcb246c508bab56ff
MikuroXina <ryosukadnak@gmail.com>
2022-11-04 08:28:52 +0900

    Fix to wrap audio wave

--- src/audio_vec.rs
+++ src/audio_vec.rs
@@ -1,6 +1,5 @@
 use std::ops::{Index, IndexMut};
 
-use itertools::Itertools;
 use serde::{Deserialize, Serialize};
 
 use self::mod_int::ModInt998244353;
@@ -151,14 +150,16 @@ impl AudioVec {
     pub fn from_pcm(path: &[i16]) -> Self {
         let emphasized = path
             .iter()
-            // バイアス
-            .map(|&x| (x as i64) + i16::MAX as i64)
-            .tuple_windows()
-            // プリエンファシス
-            .map(|(curr, next)| next - curr * 97 / 100)
-            // ポストバイアス
-            .map(|x| x + i32::MAX as i64)
-            .map(|x| ModInt998244353::new(x.try_into().unwrap_or_else(|_| panic!("{x}"))));
+            // 折り返し
+            .map(|&x| {
+                if 0 <= x {
+                    x as i64
+                } else {
+                    (x as i64) + ModInt998244353::N as i64
+                }
+            })
+            .map(|x| x.try_into().unwrap_or_else(|_| panic!("{x}")))
+            .map(ModInt998244353::new);
 
         Self {
             vec: emphasized.collect(),
